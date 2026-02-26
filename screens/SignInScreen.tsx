9c706ac3806ef07ba3110be0045c68127fa513be
olegmoshkovich <oleg.mosh@gmail.com>
2026-02-26 20:44:41 +0100

    update

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -50,7 +50,7 @@ export default function SignInScreen({
       <StatusBar barStyle="dark-content" />
 
       <View style={styles.titleContainer}>
-        <Text style={styles.titleText}>Welcome to Val Mia</Text>
+        <Text style={styles.titleText}>Welcome to ValMia</Text>
       </View>
 
       <View style={styles.formContainer}>
